1652bc6657be0b6d58d9b68aa45fd5c3f5f572a5
Kurl <kurl.palaganas@aiah.ai>
2021-07-15 23:25:46 +0800

    Update trigger_per_day.py

--- modules/conversation_flows/trigger_per_day.py
+++ modules/conversation_flows/trigger_per_day.py
@@ -1,5 +1,5 @@
-from app.modules.messenger_api.quick_replies import MessengerQuickReplies
-from app.modules.messenger_api.text import MessengerTextMessage
+from modules.messenger_api.quick_replies import MessengerQuickReplies
+from modules.messenger_api.text import MessengerTextMessage
 
 TRIGGER_PER_DAY = {
     'day2': [
@@ -38,6 +38,86 @@ TRIGGER_PER_DAY = {
                     'content_type': 'text',
                     'title': '\'Lang tulog dahil d2',
                     'payload': '\'Lang tulog dahil d2'
+                }
+            ]
+        )
+    ],
+    'day4': [
+        MessengerTextMessage(text='Kamusta naman diyan?'),
+        MessengerTextMessage(text='So eto...Pinili ni Tia K. yung 15K na apartment.'),
+        MessengerQuickReplies(
+            text='"Yung rent difference naman ay parang like I\'m paying for a better quality of life." 💁‍♀️',
+            quick_replies=[
+                {
+                    'content_type': 'text',
+                    'title': 'Tru thoooo 👏',
+                    'payload': 'Tru thoooo 👏'
+                },
+                {
+                    'content_type': 'text',
+                    'title': 'Haist di nakikinig',
+                    'payload': 'Haist di nakikinig'
+                }
+            ]
+        )
+    ],
+    'day5': [
+        MessengerTextMessage(text='Heeelllooo, may update ulet ako kay Tia K.!!! 👋'),
+        MessengerTextMessage(text='Fighter talaga yung babae na \'to. 💪'),
+        MessengerQuickReplies(
+            text='"OK," ang sinabi niya, "I have to pick myself up and make desisyon about what to do sa susunod."',
+            quick_replies=[
+                {
+                    'content_type': 'text',
+                    'title': 'GO GURL! 👏',
+                    'payload': 'GO GURL! 👏'
+                },
+                {
+                    'content_type': 'text',
+                    'title': '🤡',
+                    'payload': '🤡'
+                }
+            ]
+        )
+    ],
+    'day6': [
+        MessengerTextMessage(text='Hai mamser!!! What can I serve you today? 😗🍵🫖🧋'),
+        MessengerTextMessage(text='FAST FORWARD! Zoom zoom zoom beep beep. ⏭\nAfter 3 months, mukhang wala pang nakita ng trabaho si Tia K. habang lumiliit lang yung savings niya. 😔'),
+        MessengerQuickReplies(
+            text='So nag-decide siya na lumipat ulit sa bahay ng mga magulang niya.',
+            quick_replies=[
+                {
+                    'content_type': 'text',
+                    'title': '3 months na agad???',
+                    'payload': '3 months na agad???'
+                },
+                {
+                    'content_type': 'text',
+                    'title': 'Noooo 😭',
+                    'payload': 'Noooo 😭'
+                },
+                {
+                    'content_type': 'text',
+                    'title': 'Tama lang 🤷‍♀️',
+                    'payload': 'Tama lang 🤷‍♀️'
+                }
+            ]
+        )
+    ],
+    'day7': [
+        MessengerTextMessage(text='Bilib mo, isang linggo na kami nagkukuwentuhan! 😗✌️ Saya, noh? 🤪'),
+        MessengerQuickReplies(
+            text='Wala akong news today, may tanong lang... May idea ka pa kung paano pa kumita si Tia K.? 👀 Para sa she can pursue her passion and be independent, diba!!! 👏👏👏',
+            quick_replies=[
+                {
+                    'content_type': 'text',
+                    'title': 'Yisss, may idea ako!',
+                    'payload': 'Yisss, may idea ako!'
+                },
+                {
+                    'content_type': 'text',
+                    'title': 'Pass muna 😅😂',
+                    'payload': 'Pass muna 😅😂'
                 }
             ]
         )
